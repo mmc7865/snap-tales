fc8c0b5369766fa7549d8d108baf74463bd45f97
mmc7865 <mmc8791@gmail.com>
2025-03-05 03:19:27 +0530

    Update database connection string to use MongoDB Atlas

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
  function connect(){
-    mongoose.connect('mongodb://localhost:27017/insta')
+    mongoose.connect('mongodb+srv://ak0163797:o67Uenf7QiefRMc0@cluster0.oabm5.mongodb.net/insta')
     .then(()=>{
         console.log('server is connected to db')
     })
